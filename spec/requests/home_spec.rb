53900cb84694e88b77555b6f691e6896e144f898
k-suke39 <pz.mozarla@gmail.com>
2023-07-23 13:54:09 +0900

    🔥 rm spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,11 +2,3 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Homes' do
-  describe 'GET /top' do
-    it 'returns http success' do
-      get '/home/top'
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
