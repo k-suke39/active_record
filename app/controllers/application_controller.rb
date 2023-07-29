103b9ef70b5088dabd32291029d96eef586038f7
k-suke39 <pz.mozarla@gmail.com>
2023-07-29 16:52:39 +0900

    ⚡️ ログイン制限ページと処理と通知ができるように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
   helper_method :logged_in?
   
   private
 
   def logged_in?
     !!session[:user_id]
+  end
+  
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
