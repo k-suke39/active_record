1e368dca24a159f086e1bf7de40b046d6f94519b
k-suke39 <pz.mozarla@gmail.com>
2023-07-31 12:18:05 +0900

    ⚡️ createアクションの実装

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,4 +1,16 @@
 class Admin::UserSessionsController < ApplicationController
   layout 'admin/layouts/application'
-  def new; end
+  def new
+    @admin_user = AdminUser.new
+  end
+  
+  def create
+    @admin_user = login(params[:email], params[:password])
+
+    if @admin_user
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
