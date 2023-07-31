75a928a035b5e764afa0995e9eeb6a4a34b85bfc
k-suke39 <pz.mozarla@gmail.com>
2023-07-31 13:17:19 +0900

    ⚡️

--- test/controllers/admin/lessons_controller_test.rb
+++ test/controllers/admin/lessons_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class Admin::LessonsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get admin_lessons_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get admin_lessons_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get admin_lessons_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get admin_lessons_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get admin_lessons_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get admin_lessons_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get admin_lessons_destroy_url
+    assert_response :success
+  end
 end
