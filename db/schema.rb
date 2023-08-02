44e91b1443f509a8c8879b0f3ef26b3e7e712912
k-suke39 <pz.mozarla@gmail.com>
2023-08-03 00:38:39 +0900

    ⚡️

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema[7.0].define(version: 2023_07_30_235847) do
   create_table "admin_users", force: :cascade do |t|
     t.string "email", null: false
