2ade647becf10c435720e5fee1fbe4c3bdcfd3a0
k-suke39 <pz.mozarla@gmail.com>
2023-08-02 11:34:33 +0900

    ⚡️

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ActiveRecord
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.css_compressor = nil
   end
 end
